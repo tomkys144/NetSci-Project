8e4f352c4770db56ff59a34a33a794a29ed5efc2
Tomáš Kysela <tomkys144@gmail.com>
2025-12-03 15:01:36 +0100

    Changed to pandas

--- code/brainNet.py
+++ code/brainNet.py
@@ -1,99 +1,137 @@
 import os.path as op
 import os
-import math
+import networkx as nx
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
+import pickle
 
 
-def _extraction_data_csv(filePath):
-    file = open(filePath, "r")
-    lines = file.readlines()
-    fileDic = {}
-    keys = lines[0].split(";")
-    for k in range(len(keys)):
-        keys[k] = keys[k].replace("\n", "")
-    for i in lines[1:]:
-        currentLine = {}
-        listLine = i.split(";")
-        for j in range(1, len(listLine)):
-            currentLine[keys[j]] = float(listLine[j].replace("\n", ""))
-        fileDic[int(listLine[0])] = currentLine
-    return (fileDic)
+def _normalize_df_by_key(df: pd.DataFrame, key: str) -> pd.DataFrame:
+    minValue = df[key].min()
+    maxValue = df[key].max()
 
+    df[key] = (df[key] - minValue) / (maxValue - minValue)
 
-def _normalize_dic_by_key(dic, key):
-    minValue = math.inf
-    maxValue = -math.inf
-    for i in dic:
-        if dic[i][key] < minValue :
-            minValue = dic[i][key]
-        if dic[i][key] > maxValue :
-            maxValue = dic[i][key]
-    for i in dic:
-        dic[i][key] = (dic[i][key] - minValue) / (maxValue-minValue)
-    return(dic)
-
-def _edges2matrix(edges, numNodes, keyNode1, keyNode2, keyWeight='', undirected=True):
-    adjMatrix = np.zeros((numNodes, numNodes))
-    for edge in edges.values():
-        node1 = int(edge[keyNode1])
-        node2 = int(edge[keyNode2])
-        weight = edge[keyWeight]
-        adjMatrix[node1, node2] = weight
-        if undirected:
-            adjMatrix[node2, node1] = weight
-    return adjMatrix
+    return (df)
 
 
 class BrainNet:
     nodes = None
     edges = None
-    adjMatrix = None
+    graph = None
 
-    def __init__(self, dataset: str, useCache = True):
+    def __init__(self, dataset: str, directed: bool = False, useCache: bool = True):
         root = op.dirname(__file__)
         if op.basename(root) != "code":
             raise Exception("Unexpected file location")
-        csvEdgesPath = op.join(root,"datasets", dataset, "edges.csv")
-        csvNodesPath = op.join(root,"datasets", dataset, "nodes.csv")
+        csvEdgesPath = op.join(root, "datasets", dataset, "edges.csv")
+        csvNodesPath = op.join(root, "datasets", dataset, "nodes.csv")
 
-        self.nodes = _extraction_data_csv(csvNodesPath)
-        self.edges = _extraction_data_csv(csvEdgesPath)
+        print("Loadin nodes...")
+        self.nodes = pd.read_csv(
+            csvNodesPath,
+            header=0,
+            delimiter=";",
+            index_col='id',
+            dtype={
+                'pos_x': np.float64,
+                'pos_y': np.float64,
+                'pos_z': np.float64,
+                'degree': np.int32,
+                'isAtSampleBorder': np.bool
+            }
+        )
 
-        # normalization between 0 and 100
+        print("Loadin edges...")
+        self.edges = pd.read_csv(
+            csvEdgesPath,
+            header=0,
+            delimiter=";",
+            index_col='id',
+            usecols=(
+                'id',
+                'node1id',
+                'node2id',
+                'avgRadiusAvg'
+            ),
+            dtype={
+                'node1id': np.int32,
+                'node2id': np.int32,
+                'avgRadiusAvg': np.float64
+            }
+        )
+
+        print("Normalizing nodes...")
         nodesKeysToNormalize = ["pos_x", "pos_y", "pos_z"]
         for key in nodesKeysToNormalize:
-            self.nodes = _normalize_dic_by_key(self.nodes, key)
+            self.nodes = _normalize_df_by_key(self.nodes, key)
 
+        print("Normalizing edges...")
         edgesKeysToNormalize = ["avgRadiusAvg"]
         for key in edgesKeysToNormalize:
-            self.edges = _normalize_dic_by_key(self.edges, key)
+            self.edges = _normalize_df_by_key(self.edges, key)
 
+        print("Creating graph...")
         if useCache:
+            if directed:
+                cachePath = op.join(root, "cache", f'{dataset}_dir.pickle')
+            else:
+                cachePath = op.join(root, "cache", f'{dataset}_udir.pickle')
+
             try:
-                self.adjMatrix = np.load(op.join(root,"cache", f'{dataset}.npy'))
+                self.graph = pickle.load(open(cachePath, 'rb'))
             except FileNotFoundError:
-                self.adjMatrix = _edges2matrix(self.edges, len(self.nodes), 'node1id', 'node2id', 'avgRadiusAvg')
-                if not op.exists(op.join(root,"cache")):
-                    os.makedirs(op.join(root,"cache"))
-                np.save(op.join(root,"cache", f'{dataset}.npy'), self.adjMatrix)
+                if directed:
+                    self.graph = nx.from_pandas_edgelist(
+                        self.edges,
+                        source='node1id',
+                        target='node2id',
+                        edge_attr='avgRadiusAvg',
+                        create_using=nx.DiGraph()
+                    )
+                else:
+                    self.graph = nx.from_pandas_edgelist(
+                        self.edges,
+                        source='node1id',
+                        target='node2id',
+                        edge_attr='avgRadiusAvg',
+                    )
+                nx.set_node_attributes(self.graph, self.nodes.to_dict('index'))
+                if not op.exists(op.join(root, "cache")):
+                    os.makedirs(op.join(root, "cache"))
+                pickle.dump(self.graph, open(cachePath, 'wb'))
         else:
-            self.adjMatrix = _edges2matrix(self.edges, len(self.nodes), 'node1id', 'node2id', 'avgRadiusAvg')
+            if directed:
+                self.graph = nx.from_pandas_edgelist(
+                    self.edges,
+                    source='node1id',
+                    target='node2id',
+                    edge_attr='avgRadiusAvg',
+                    create_using=nx.DiGraph()
+                )
+            else:
+                self.graph = nx.from_pandas_edgelist(
+                    self.edges,
+                    source='node1id',
+                    target='node2id',
+                    edge_attr='avgRadiusAvg',
+                )
+            nx.set_node_attributes(self.graph, self.nodes.to_dict('index'))
 
-
-    def visualize(self, outputFile='', show=True):
+    def visualize(self, outputFile:str='', show:bool=True):
         fig, ax = plt.subplots()
         ax = fig.add_subplot(projection='3d')
 
         print("Graphing...")
         progress = 0
-        for e in self.edges:
-            X = [self.nodes[self.edges[e]['node1id']]['pos_x'], self.nodes[self.edges[e]['node2id']]['pos_x']]
-            Y = [self.nodes[self.edges[e]['node1id']]['pos_y'], self.nodes[self.edges[e]['node2id']]['pos_y']]
-            Z = [self.nodes[self.edges[e]['node1id']]['pos_z'], self.nodes[self.edges[e]['node2id']]['pos_z']]
-            ax.plot(X, Y, Z, color='red', alpha=0.4, linewidth=self.edges[e]['avgRadiusAvg'] * 5)
+        for source, target, attr in self.graph.edges(data=True):
+            X = [self.graph.nodes[source]['pos_x'] , self.graph.nodes[target]['pos_x']]
+            Y = [self.graph.nodes[source]['pos_y'] , self.graph.nodes[target]['pos_y']]
+            Z = [self.graph.nodes[source]['pos_z'] , self.graph.nodes[target]['pos_z']]
+            ax.plot(X, Y, Z, linewidth=attr['avgRadiusAvg'] * 5, color='red', alpha=0.4)
             if progress % 5000 == 0:
-                print(str(100 * progress / len(self.edges)) + "%")
+                print(str(100 * progress / len(self.edges)) + "%         \r")
             progress += 1
 
         print("Done!")
@@ -106,5 +144,5 @@ class BrainNet:
 
 
 if __name__ == "__main__":
-    brainNet = BrainNet("synthetic_graph_1")
+    brainNet = BrainNet("CD1-E_no2")
     brainNet.visualize()
