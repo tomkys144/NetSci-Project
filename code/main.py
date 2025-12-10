aacc96e6ba53c4ea41564cf32973d92f889b4d13
Tomáš Kysela <tomkys144@gmail.com>
2025-12-10 18:58:29 +0100

    Printing from sides

--- code/main.py
+++ code/main.py
@@ -11,13 +11,18 @@ def main():
     # brainNet.visualize(outputFile="res.png", show=False)
     brainNet.get_gt()
     print("Printing raw graph...")
-    # brainNet.draw_gt(f"graph-{dataset}.png")
+    brainNet.draw_gt(f"graph-{dataset}-xy.png", coords=(0, 1))
+    brainNet.draw_gt(f"graph-{dataset}-xz.png", coords=(0, 2))
+    brainNet.draw_gt(f"graph-{dataset}-yz.png", coords=(1, 2))
 
     # Find communities
     print("Community detection...")
     sbmState = comunities.sbm(brainNet, nmcmc=10)
     print("Printing results...")
     comunities.draw_sbm(sbmState, f"sbm-{dataset}.png")
+    comunities.draw_sbm_shape(sbmState, brainNet, f"sbm-{dataset}-xy.png", coords=(0, 1))
+    comunities.draw_sbm_shape(sbmState, brainNet, f"sbm-{dataset}-xz.png", coords=(0, 2))
+    comunities.draw_sbm_shape(sbmState, brainNet,f"sbm-{dataset}-yz.png", coords=(1,2))
     print("Done")
 
 
