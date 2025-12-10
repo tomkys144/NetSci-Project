a62d099c52b72629273805f2812e844514b29537
Tomáš Kysela <tomkys144@gmail.com>
2025-12-10 17:57:48 +0100

    Added community detection

--- code/brainNet.py
+++ code/brainNet.py
@@ -1,10 +1,12 @@
-import os.path as op
 import os
+import os.path as op
+import pickle
+
+import graph_tool.all as gt
 import networkx as nx
 import numpy as np
-from matplotlib import pyplot as plt
 import pandas as pd
-import pickle
+from matplotlib import pyplot as plt
 
 
 def _normalize_df_by_key(df: pd.DataFrame, key: str) -> pd.DataFrame:
@@ -16,8 +18,36 @@ def _normalize_df_by_key(df: pd.DataFrame, key: str) -> pd.DataFrame:
     return df
 
 
+def _get_prop_type(value, key=None):
+    """
+    Performs typing and value conversion for the graph_tool PropertyMap class.
+    If a key is provided, it also ensures the key is in a format that can be
+    used with the PropertyMap. Returns a tuple, (type name, value, key)
+    """
+    # Deal with the value
+    if isinstance(value, bool):
+        tname = 'bool'
+
+    elif isinstance(value, int):
+        tname = 'float'
+        value = float(value)
+
+    elif isinstance(value, float):
+        tname = 'float'
+
+    elif isinstance(value, dict):
+        tname = 'object'
+
+    else:
+        tname = 'string'
+        value = str(value)
+
+    return tname, value, key
+
+
 class BrainNet:
     graph: nx.Graph | nx.DiGraph
+    gtGraph: gt.Graph
 
     def __init__(self, dataset: str, directed: bool = False, useCache: bool = True):
         root = op.dirname(__file__)
@@ -139,7 +169,91 @@ class BrainNet:
         if show:
             plt.show()
 
+    def get_gt(self):
+        print("Converting NX graph to GT...")
+        self.gtGraph = gt.Graph(directed=self.graph.is_directed())
+
+        # 1. Graph Properties
+        for key, value in self.graph.graph.items():
+            tname, value, key = _get_prop_type(value, key)
+            self.gtGraph.graph_properties[key] = self.gtGraph.new_graph_property(tname)
+            self.gtGraph.graph_properties[key] = value
+
+        # 2. Register Vertex Properties
+        nprops = set()
+        for node, data in self.graph.nodes(data=True):
+            for key, val in data.items():
+                if key in nprops: continue
+                tname, _, key = _get_prop_type(val, key)
+                self.gtGraph.vertex_properties[key] = self.gtGraph.new_vertex_property(tname)
+                nprops.add(key)
+
+        # Add 'id' property explicitly
+        self.gtGraph.vertex_properties['id'] = self.gtGraph.new_vertex_property('string')
+
+        # 3. Register Edge Properties
+        eprops = set()
+        for src, dst, data in self.graph.edges(data=True):
+            for key, val in data.items():
+                if key in eprops: continue
+                tname, _, key = _get_prop_type(val, key)
+                self.gtGraph.edge_properties[key] = self.gtGraph.new_edge_property(tname)
+                eprops.add(key)
+
+        # 4. Add Vertices and Data
+        vertices = {}
+        for node, data in self.graph.nodes(data=True):
+            v = self.gtGraph.add_vertex()
+            vertices[node] = v
+
+            # Manually set ID
+            self.gtGraph.vp['id'][v] = str(node)
+
+            for key, value in data.items():
+                # Ensure we cast the value exactly as we determined the type
+                _, casted_val, _ = _get_prop_type(value, key)
+                self.gtGraph.vp[key][v] = casted_val
+
+        # 5. Add Edges and Data
+        for src, dst, data in self.graph.edges(data=True):
+            e = self.gtGraph.add_edge(vertices[src], vertices[dst])
+            for key, value in data.items():
+                _, casted_val, _ = _get_prop_type(value, key)
+                self.gtGraph.ep[key][e] = casted_val
+
+        # 6. Create Position Vector for Visualization
+        if "pos" not in self.gtGraph.vp and "pos_x" in self.gtGraph.vp:
+            pos = self.gtGraph.new_vertex_property("vector<double>")
+
+            # .a returns the numpy array view of the property
+            px = self.gtGraph.vp["pos_x"].a
+            py = self.gtGraph.vp["pos_y"].a
+            pz = self.gtGraph.vp["pos_z"].a
+
+            for v in self.gtGraph.vertices():
+                idx = int(v)
+                pos[v] = [px[idx], py[idx], pz[idx]]
+
+            self.gtGraph.vp["pos"] = pos
+
+    def draw_gt(self, outputFile: str = ""):
+        if outputFile:
+            gt.graph_draw(self.gtGraph,
+                          pos=self.gtGraph.vp["pos"],
+                          output_size=(8000, 8000),
+                          vertex_size=5,
+                          output=outputFile)
+
+        else:
+            gt.graph_draw(
+                self.gtGraph,
+                pos=self.gtGraph.vp["pos"],
+                edge_pen_width=gt.prop_to_size(self.gtGraph.ep["avgRadiusAvg"], mi=0, ma=5)
+            )
+
 
 if __name__ == "__main__":
-    brainNet = BrainNet("synthetic_graph_1", directed=False, useCache=False)
-    brainNet.visualize()
+    brainNet = BrainNet("CD1-E_no2", directed=False, useCache=False)
+    brainNet.get_gt()
+    brainNet.draw_gt(outputFile="graph.png")
+    print("done")
--- code/comunities.py
+++ code/comunities.py
@@ -1,1 +1,31 @@
-import networkx as nx
+import graph_tool.all as gt
+import numpy as np
+
+from brainNet import BrainNet
+
+
+def sbm(brainNet: BrainNet, nmcmc=100):
+    if not hasattr(brainNet, "gtGraph"):
+        brainNet.get_gt()
+
+    state = gt.minimize_nested_blockmodel_dl(
+        brainNet.gtGraph,
+        state_args=dict(
+            recs=[brainNet.gtGraph.ep.avgRadiusAvg],
+            rec_types=["discrete-binomial"]
+        )
+    )
+
+    print("Calculating MCMC...")
+    for i in range(nmcmc):  # this should be sufficiently large
+        # if (i / nmcmc * 100) % 1 == 0:
+            # print("Progress: ", (i / nmcmc * 100), "%")
+        state.multilevel_mcmc_sweep(beta=np.inf, niter=10)
+
+    return state
+
+
+def draw_sbm(state, outputFile: str = ""):
+    state.draw(
+        output=outputFile
+    )
