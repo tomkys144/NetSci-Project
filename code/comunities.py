946880a5c0fe6b72406be3e38891f8945b91133c
Tomáš Kysela <tomkys144@gmail.com>
2025-12-11 17:19:00 +0100

    Added command line arguments

--- code/comunities.py
+++ code/comunities.py
@@ -1,4 +1,5 @@
 import graph_tool.all as gt
+from graph_tool.draw import BlockState
 import numpy as np
 
 from brainNet import BrainNet
@@ -23,14 +24,16 @@ def sbm(brainNet: BrainNet, nmcmc=100):
     return state
 
 
-def draw_sbm(state, outputFile: str = ""):
+def draw_sbm(state, outputFile: str):
     state.draw(
-        output=outputFile
+        layout="radial",  # Forces radial/circular layout
+        beta=0.9,  # High bundling makes radial structure clearer
+        output=outputFile,
+        output_size=(1500, 1500),  # Larger size prevents overlapping text/nodes
     )
 
-
-def draw_sbm_shape(state, brainNet: BrainNet, outputFile: str = "", coords=(0,1)):
-    lstate = state.get_levels()[0]
+def draw_sbm_shape(state: gt.NestedBlockState, brainNet: BrainNet, outputFile: str, coords=(0,1), layer=0):
+    pstate = state.project_level(layer)
 
     pos3d = brainNet.gtGraph.vp.pos.get_2d_array(pos=[0, 1, 2])
     pos2d = pos3d[list(coords), :]
@@ -38,7 +41,8 @@ def draw_sbm_shape(state, brainNet: BrainNet, outputFile: str = "", coords=(0,1)
     pos = brainNet.gtGraph.new_vertex_property("vector<double>")
     pos.set_2d_array(pos2d)
 
-    lstate.draw(
+    pstate.draw(
         pos=pos,
-        output=outputFile
+        output=outputFile,
+        output_size=(1500, 1500),
     )
